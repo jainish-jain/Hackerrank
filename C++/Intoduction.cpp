f2944d34bdbaab366961b2f19e71e1cda0bc97dd
Jainish Jain <62622479+jainish-jain@users.noreply.github.com>
2020-07-19 16:49:18 +0530

    init

--- C++/Intoduction.cpp
+++ C++/Intoduction.cpp
@@ -38,3 +38,122 @@ Float ("%f"): 32 bit real value
 Double ("%lf"): 64 bit real value
 */
 
+#include <iostream>
+#include <cstdio>
+#include <iomanip>      // std::setprecision
+using namespace std;
+using std::cin;
+using std::cout;
+using std::endl;
+using std::fixed;
+int main() {
+    int a;
+long b;
+char c;
+float d;
+double e;
+cin >> a >> b >> c >> d >> e;
+cout<< a << '\n' << b << '\n' << c << '\n';
+cout << fixed << setprecision(3) << d << '\n';
+cout << fixed <<setprecision(9) << e << '\n';
+
+return 0;
+}
+
+#include <iostream>
+int main() {
+    int a; long b; char c; float d; double e;
+    
+    scanf("%i %li %c %f %lf",&a,&b,&c,&d,&e);
+    printf("%i\n%li\n%c\n%.03f\n%.09lf\n",a,b,c,d,e);
+    
+    return 0;
+}
+
+
+//Conditional Statements
+
+
+#include <bits/stdc++.h>
+
+using namespace std;
+
+
+
+int main()
+{
+    int n;
+    cin >> n;
+    cin.ignore(numeric_limits<streamsize>::max(), '\n');
+
+    if (n==1){cout<<"one";}
+    else if(n==2){cout<<"two";}
+    else if (n==3){cout<<"three";}
+    else if (n==4){cout<<"four";}
+    else if (n==5){cout<<"five";}
+    else if (n==6){cout<<"six";}
+    else if (n==7){cout<<"seven";}
+    else if (n==8){cout<<"eight";}
+    else if (n==9){cout<<"nine";}
+    else {cout<<"Greater than 9";}
+
+    return 0;
+}
+
+//For Loop
+
+#include <iostream>
+#include <cstdio>
+using namespace std;
+
+int main() {
+    int i,j;
+    string nm[12]={"","one","two","three","four","five","six","seven","eight","nine","odd","even"};
+    cin>>i>>j;
+    for (i;i<=j;i++){
+        if (i>0 && i<10){
+            cout<<nm[i]<<'\n';
+        }
+        else if (i%2==0){
+            cout<<nm[11]<<'\n';
+        }
+        else {
+            cout<<nm[10]<<'\n';
+        }
+    }
+
+    return 0;
+}
+
+//Functions
+
+#include <iostream>
+#include <cstdio>
+using namespace std;
+
+/*
+Add `int max_of_four(int a, int b, int c, int d)` here.
+*/
+
+int max_of_four(int a,int b,int c,int d){
+    int s=a;
+    if (b>s){s=b;}
+    if(c>s){s=c;}
+    if(d>s){s=d;}
+    return s;
+}
+int main() {
+    int a, b, c, d;
+    scanf("%d %d %d %d", &a, &b, &c, &d);
+    int ans = max_of_four(a, b, c, d);
+    printf("%d", ans);
+    
+    return 0;
+}
+
+
+int max_of_four(int a,int b,int c,int d)
+{ 
+    int x,y; x=max(a,b); y=max(c,d);
+    return max(x,y); 
+ }
